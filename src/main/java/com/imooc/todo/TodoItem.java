6dd8b4e91961ea0f9f7c400bbe13939aa3de23b1
yexudong <ye850570138@sina.com>
2018-06-26 11:14:32 +0800

    Initial commit

---
+++ src/main/java/com/imooc/todo/TodoItem.java
@@ -0,0 +1,32 @@
+package com.imooc.todo;
+
+public class TodoItem {
+
+    private String name;
+    private boolean hasDone;
+
+    public TodoItem(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isHasDone() {
+        return hasDone;
+    }
+
+    public void setHasDone(boolean hasDone) {
+        this.hasDone = hasDone;
+    }
+
+    @Override
+    public String toString() {
+        return "name is "+name+" and has done is "+hasDone;
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,12 @@
+<%-- Created by IntelliJ IDEA. --%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+<h1>Hello World!!!Like this</h1>
+<h2>yoo yoo yoo isn't my language</h2>
+
+</body>
+</html>
